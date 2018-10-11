1f505ec8cd2f675a42b341f05932a8356d48dee3
Kerimov Ahmed <kerimov.main@gmail.com>
2018-10-11 12:56:17 +0300

    Fix indentation in lab_09_1/test.c

--- lab_09/lab_09_1/test.c
+++ lab_09/lab_09_1/test.c
@@ -13,17 +13,25 @@ bool test_my_strspn_empty_sym(void);
 
 bool (*tests[])(void) = {
 	test_my_strspn_simple,
-		test_my_strspn_zero, // omg style check
-		test_my_strspn_full,
-		test_my_strspn_empty_str,
-		test_my_strspn_empty_sym
-};
+	test_my_strspn_zero,
+	test_my_strspn_full,
+	test_my_strspn_empty_str,
+	test_my_strspn_empty_sym };
+
+const char *names[] = {
+	"test_my_strspn_simple",
+	"test_my_strspn_zero",
+	"test_my_strspn_full",
+	"test_my_strspn_empty_str",
+	"test_my_strspn_empty_sym" };
 
 int main(void)
 {
 	size_t passed = 0;
 
 	for (size_t i = 0; i != TESTS; ++i)
+	{
+		fprintf(stderr, "%s: ", names[i]);
 		if (tests[i]())
 		{
 			fprintf(stderr, "passed\n");
@@ -31,6 +39,7 @@ int main(void)
 		}
 		else
 			fprintf(stderr, "failed\n");
+	}
 
 	fprintf(stderr, "passed %lu/%d\n", passed, TESTS);
 	return passed != TESTS;
@@ -38,35 +47,30 @@ int main(void)
 
 bool test_my_strspn_simple(void)
 {
-	fprintf(stderr, "test_my_strspn_simple: ");
 	const char *str = "123th", *sym = "0123456789";
 	return my_strspn(str, sym) == strspn(str, sym);
 }
 
 bool test_my_strspn_zero(void)
 {
-	fprintf(stderr, "test_my_strspn_zero: ");
 	const char *str = "alphavezzy", *sym = "0123456789";
 	return my_strspn(str, sym) == strspn(str, sym);
 }
 
 bool test_my_strspn_full(void)
 {
-	fprintf(stderr, "test_my_strspn_full: ");
 	const char *str = "123000", *sym = "0123456789";
 	return my_strspn(str, sym) == strspn(str, sym);
 }
 
 bool test_my_strspn_empty_str(void)
 {
-	fprintf(stderr, "test_my_strspn_empty_str ");
 	const char *str = "", *sym = "abcdef";
 	return my_strspn(str, sym) == strspn(str, sym);
 }
 
 bool test_my_strspn_empty_sym(void)
 {
-	fprintf(stderr, "test_my_strspn_empty_sym: ");
 	const char *str = "abcdef", *sym = "";
 	return my_strspn(str, sym) == strspn(str, sym);
 }
