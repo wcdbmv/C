8853f8da4f78315dedc19720cfd689137038c2fa
Kerimov Ahmed <kerimov.main@gmail.com>
2018-11-14 19:08:54 +0300

    Completely fix codestyle

--- lab_11/test.c
+++ lab_11/test.c
@@ -5,7 +5,7 @@
 #include <string.h> // strcmp
 #include <stdio.h>
 
-typedef uint8_t U;
+typedef uint8_t u_t;
 
 bool test_my_snprintf_simple(void);
 bool test_my_snprintf_overflow(void);
@@ -18,16 +18,15 @@ bool (*tests[])(void) = {
 	test_my_snprintf_all_together,
 	test_my_snprintf_nullptr };
 
-extern inline void print_header(const char *s);
-extern inline bool print_result(const bool res);
-extern inline bool print_passed(const U passed, const U total);
+extern inline bool print_result(const char *func, const bool res);
+extern inline bool print_passed(const u_t passed, const u_t total);
 
 int main(void)
 {
-	const U TESTS = sizeof tests / sizeof tests[0];
-	U passed = 0;
+	const u_t TESTS = sizeof tests / sizeof tests[0];
+	u_t passed = 0;
 
-	for (U i = 0; i != TESTS; ++i)
+	for (u_t i = 0; i != TESTS; ++i)
 		passed += tests[i]();
 
 	return print_passed(passed, TESTS);
@@ -35,19 +34,14 @@ int main(void)
 
 //——————————————————————————————————————————————————————————————————————— print
 
-inline void print_header(const char *s)
+inline bool print_result(const char *func, const bool res)
 {
-	fprintf(stderr, "%s: ", s);
-}
-
-inline bool print_result(const bool res)
-{
-	fprintf(stderr, "%s\n", res ? "passed" : "failed");
+	fprintf(stderr, "%s: %s\n", func, res ? "passed" : "failed");
 	return res;
 }
 
 // success — 0
-inline bool print_passed(const U passed, const U total)
+inline bool print_passed(const u_t passed, const u_t total)
 {
 	fprintf(stderr, "passed %hhu/%hhu\n", passed, total);
 	return passed != total;
@@ -57,8 +51,6 @@ inline bool print_passed(const U passed, const U total)
 
 bool test_my_snprintf_simple(void)
 {
-	print_header(__func__);
-
 	size_t n = 10;
 	char s1[n], s2[n];
 	const char *format = "uhh";
@@ -68,13 +60,11 @@ bool test_my_snprintf_simple(void)
 
 	bool res = rc1 == rc2 && !strcmp(s1, s2);
 
-	return print_result(res);
+	return print_result(__func__, res);
 }
 
 bool test_my_snprintf_overflow(void)
 {
-	print_header(__func__);
-
 	size_t n = 5;
 	char s1[n], s2[n];
 	const char *format = "billion";
@@ -84,13 +74,11 @@ bool test_my_snprintf_overflow(void)
 
 	bool res = rc1 == rc2 && !strcmp(s1, s2);
 
-	return print_result(res);
+	return print_result(__func__, res);
 }
 
 bool test_my_snprintf_all_together(void)
 {
-	print_header(__func__);
-
 	size_t n = 20;
 	char s1[n], s2[n];
 	const char *format = "hx%hx%%%s%i%__i";
@@ -103,15 +91,13 @@ bool test_my_snprintf_all_together(void)
 
 	bool res = rc1 == rc2 && !strcmp(s1, s2);
 
-	return print_result(res);
+	return print_result(__func__, res);
 }
 
 
 bool test_my_snprintf_nullptr(void)
 {
-	print_header(__func__);
-
 	bool res = my_snprintf(NULL, 0, NULL) == MY_SNPRINTF_ERROR;
 
-	return print_result(res);
+	return print_result(__func__, res);
 }
